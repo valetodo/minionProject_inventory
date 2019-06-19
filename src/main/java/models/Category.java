6b4dc5e82a3f8deed760bef69ad3c851af263cb0
yerel <estalinyerel123@gmail.com>
2019-06-19 13:39:33 -0700

    add class Category to models package

---
+++ src/main/java/models/Category.java
@@ -0,0 +1,22 @@
+package models;
+
+public class Category {
+    private String name;
+    private String measurement;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMeasurement() {
+        return measurement;
+    }
+
+    public void setMeasurement(String measurement) {
+        this.measurement = measurement;
+    }
+}
