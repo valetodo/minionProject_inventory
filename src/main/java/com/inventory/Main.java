7b5d4b62d9033269cdecc9ae674666277b5dff53
Yerel  Hurtado <estalin.hurtado@jalasoft.com>
2019-07-29 22:25:58 -0400

    chage to maven project

---
+++ src/main/java/com/inventory/Main.java
@@ -0,0 +1,15 @@
+package com.inventory;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+}
